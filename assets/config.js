6dc36a6edd9466bb4baf37b30b20a46e0959028c
devnull <devnull@cpanel.net>
2018-05-21 10:32:48 -0500

    Initial import of website

---
+++ assets/config.js
@@ -0,0 +1,50 @@
+// cpanel - site_templates/vcard_dev/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "1-800-ACTUALLY-CHAD",
+        fax: "",
+        email: "secret.chad@mywebsite.com",
+        address: "12345 Sesame St\nHouston TX 77003",
+        social: [
+            
+            {
+                icon: 'facebook',
+                link: "https:\/\/www.cpanel.com"
+            },
+            
+            
+            {
+                icon: 'twitter',
+                link: "https:\/\/twitter.com\/twitterdev"
+            },
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "",
+            title: "Englebert Humperdink",
+            subtitle: "Master Developer",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "",
+            portraitImage: ""
+        }
+    ]
+};
